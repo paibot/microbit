5e0f19bc818f4c18dc550eae926ae187dc44f8c2
paibot <jinliang.wang@live.com>
2019-09-14 08:50:20 +0800

    Added motion sensor support; Adjusted the line follow sensor pairs according to the hardware.

---
+++ shims.d.ts
@@ -0,0 +1,17 @@
+// Auto-generated. Do not edit.
+declare namespace motion {
+
+    /**
+     * Init moiton sensors
+     */
+    //% shim=motion::init
+    function init(): boolean;
+
+    /**
+     * Read rotation
+     */
+    //% shim=motion::rotation
+    function rotation(axis: int32): number;
+}
+
+// Auto-generated. Do not edit. Really.
